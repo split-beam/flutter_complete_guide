85c261e1da9c4611c70daeca6968951883f1cc71
Quentin <qvdeutekom@icloud.com>
2022-06-16 12:42:52 +0200

    Added Maps & Lists

--- lib/answer.dart
+++ lib/answer.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,8 @@ class Answer extends StatelessWidget {
       width: double.infinity,
       child: RaisedButton(
         color: Colors.blue,
-        child: Text('Answer 1'),
+        textColor: Colors.white,
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
